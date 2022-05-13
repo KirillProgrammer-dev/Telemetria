bb5fb4bbd5ee66a0ecf5ebfccdfa07b427bf09de
Kirill <kirilkenig@gmail.com>
2022-05-13 05:37:53 -0700

    t

--- Telemetria/resources/js/views-vue/Periudmenu.vue
+++ Telemetria/resources/js/views-vue/Periudmenu.vue
@@ -1,29 +1,62 @@
 <template>
-    <div style="width: 80%; padding: 20px; margin: 20px auto; background-color: #f2f2f2">
-        <h1>Выберите временные промежутки</h1>
-        <form>
-            <v-text-field type="number" minlength="10" maxlength="10" :counter="10" v-model="numbers" :error-messages="nameErrors"  label="Начало" required
-                ></v-text-field>
-            <v-text-field type="number" minlength="10" maxlength="10" :counter="10" v-model="numbers" :error-messages="numbers" label="Конец" required
-                ></v-text-field>
-            
+    <v-stepper v-model="e1">
+        <v-stepper-header>
+            <v-stepper-step :complete="e1 > 1" step="1">
+                Выбор временных рамок
+            </v-stepper-step>
 
-            <v-btn class="mr-4" @click="submit">
-                Подтвердить
-            </v-btn>
-            <v-btn @click="clear">
-                Очистить
-            </v-btn>
-        </form>
-    </div>
+            <v-divider></v-divider>
+
+            <v-stepper-step :complete="e1 > 2" step="2">
+                Выбор камеры
+            </v-stepper-step>
+        </v-stepper-header>
+
+        <v-stepper-items>
+            <v-stepper-content step="1">
+                <v-card class="mb-12" color="grey lighten-1" height="200px">
+                    <v-range-slider
+                    hint="Im a hint"
+                    max="50"
+                    min="-50"
+                    ></v-range-slider>
+                </v-card>
+
+                <v-btn color="primary" @click="e1 = 2">
+                    Далее
+                </v-btn>
+
+                <v-btn text>
+                    Отмена
+                </v-btn>
+            </v-stepper-content>
+
+            <v-stepper-content step="2">
+                <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+
+                <v-btn color="primary" @click="e1 = 3">
+                    
+                </v-btn>
+
+                <v-btn text>
+                    Отмена
+                </v-btn>
+            </v-stepper-content>
+        </v-stepper-items>
+    </v-stepper>
 </template>
 
 
 <script>
-    export default {
-        name: "Menu",
-        data: () => ({}),
-        methods: {}
-    }
+export default {
+    name: "Menu",
+    data: () => ({
+        time: null,
+        menu2: false,
+        modal2: false,
+        e1: 1,
+    }),
+    methods: {}
+}
 
 </script>
