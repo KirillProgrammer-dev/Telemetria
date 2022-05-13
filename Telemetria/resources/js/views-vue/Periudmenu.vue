e387637598eb6c63e7cfa13cb2c9433e7b177402
GUFFYch <koromit15@gmail.com>
2022-05-13 14:12:29 +0300

    123

---
+++ Telemetria/resources/js/views-vue/Periudmenu.vue
@@ -0,0 +1,29 @@
+<template>
+    <div style="width: 80%; padding: 20px; margin: 20px auto; background-color: #f2f2f2">
+        <h1>Выберите временные промежутки</h1>
+        <form>
+            <v-text-field type="number" minlength="10" maxlength="10" :counter="10" v-model="numbers" :error-messages="nameErrors"  label="Начало" required
+                @input="$v.numbers.$touch()" @blur="$v.numbers.$touch()"></v-text-field>
+            <v-text-field type="number" minlength="10" maxlength="10" :counter="10" v-model="numbers" :error-messages="numbers" label="Конец" required
+                @input="$v.numbers.$touch()" @blur="$v.numbers.$touch()"></v-text-field>
+            
+
+            <v-btn class="mr-4" @click="submit">
+                Подтвердить
+            </v-btn>
+            <v-btn @click="clear">
+                Очистить
+            </v-btn>
+        </form>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: "Menu",
+        data: () => ({}),
+        methods: {}
+    }
+
+</script>
